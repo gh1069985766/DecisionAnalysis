0fc92d6cd392e4b69a09bf4214686e64357a0bca
zhuhm <zhmhaha@outlook.com>
2019-04-22 09:44:31 +0800

    新增爬虫关键字自定义功能

--- src/main/java/com/decisionanalysis/project/monitor/job/task/ExecPyThread.java
+++ src/main/java/com/decisionanalysis/project/monitor/job/task/ExecPyThread.java
@@ -17,18 +17,18 @@ public class ExecPyThread extends Thread{
     @Override
     public void run() {
         try {
-//            String[] arguments = new String[] {"python","/home/crawler/mainManager.py"};
-//            Process process = Runtime.getRuntime().exec(arguments);
-//            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//
-//            String line = null;
-//            while ((line = in.readLine()) != null) {
-//                System.out.println(line);  //在java编译器中打印.py文件的执行结果
-//            }
-//            in.close();
-//            int re = process.waitFor();//因为是process这个进程调用.py文件， 所以要将当前进程阻塞等待.py文件执行完毕， 若果.py文件成功运行完毕，此方法将返回0，代表执行成功
-//            System.out.println(re);
-//
+            String[] arguments = new String[] {"python","/home/crawler/mainManager.py"};
+            Process process = Runtime.getRuntime().exec(arguments);
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);  //在java编译器中打印.py文件的执行结果
+            }
+            in.close();
+            int re = process.waitFor();//因为是process这个进程调用.py文件， 所以要将当前进程阻塞等待.py文件执行完毕， 若果.py文件成功运行完毕，此方法将返回0，代表执行成功
+            System.out.println(re);
+
 
 //            super.run();
         } catch (Exception e) {
